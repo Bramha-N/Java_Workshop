6cbd43538c549bc6afed1c0aadc4a2eef9898e74
Bramha <Bramha>
2020-09-02 09:51:40 +0530

    Basic Java by using shell

---
+++ Ifelse.java
@@ -0,0 +1,32 @@
+//Ood even program		
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String ans = "";
+        if (N%2 == 1 || (N>6 && N <=20)){
+            ans = "Weird";
+        }else{
+            ans = "Not Weird";
+        }       
+        if (N%2 == 0 && (N>20 || N<6)){
+            ans = "Not Weird";
+        }else{
+            ans = "Weird";
+        }
+        System.out.println(ans);
+        scanner.close();
+    }
+}
